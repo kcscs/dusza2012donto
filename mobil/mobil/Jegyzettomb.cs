046e3b46c7b4babe4fcf142b47bbf4f69d048f3e
kcscs <kcscs10@gmail.com>
2019-02-21 11:20:18 +0100

    j

---
+++ mobil/mobil/Jegyzettomb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobil
+{
+    class Jegyzettomb : Alkalmazas
+    {
+        int cursorPos = 0;
+        public override void Input(char c) {
+            if(c == '#') {
+                cursorPos--;
+                Program.Write(cursorPos, 0, " ");
+            } else {
+                Program.Write(cursorPos, 0, c.ToString());
+                ++cursorPos;
+            }
+        }
+        public override void Megnyit() {
+            Program.Torol();
+        }
+    }
+}
