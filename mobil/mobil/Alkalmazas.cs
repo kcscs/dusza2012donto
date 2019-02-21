d79c14c94225940c4efe731a8779fb090b1fcedb
kcscs <kcscs10@gmail.com>
2019-02-21 09:55:38 +0100

    alk

---
+++ mobil/mobil/Alkalmazas.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mobil
+{
+    abstract class Alkalmazas
+    {
+        public abstract void Input(char c);
+    }
+}
