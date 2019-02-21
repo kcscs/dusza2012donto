f260f52b76132e27573dfcdc8e51324c50c01740
Adam <zsoriadam10@gmail.com>
2019-02-21 09:17:41 +0100

    vmi

--- mobil/mobil/telefonkonyv.cs
+++ mobil/mobil/telefonkonyv.cs
@@ -8,5 +8,21 @@ namespace mobil
 {
     class telefonkonyv
     {
+        string tSzam;
+        string nev;
+        char parancs;
+        Dictionary<string ,string> nevjegyzekek=new Dictionary<string, string>();
+        public void bemenet(char input)
+        {
+            parancs = input;
+        }
+        public void nLetrehoz(string ujSzam, string ujNev)
+        {
+            nevjegyzekek.Add(ujSzam, ujNev);
+        }
+        public void nTorol(string tSzam, string tNev)
+        {
+            nevjegyzekek.Remove(tSzam);
+        }
     }
 }
